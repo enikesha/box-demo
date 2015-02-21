ca942923ea349a09f4b7d5d6fd5341384e6addf9
Innocenty Enikeew <enikesha@gmail.com>
2015-02-21 11:53:20 +0300

    Bootstrap starter template

--- box_demo/urls.py
+++ box_demo/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'box_demo.views.home', name='home'),
+    url(r'^$', 'main.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
